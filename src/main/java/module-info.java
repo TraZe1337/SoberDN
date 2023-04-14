007302ec0ac5e7eadc756982224e37d88843c220
MichaelKoscheck <michael.koscheck@gmail.com>
2023-04-14 23:08:57 +0200

    qr library implementation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.example.soberdn {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.slf4j;
-
+    requires com.google.zxing;
+    requires com.google.zxing.javase;
 
     opens com.example.soberdn to javafx.fxml;
     exports com.example.soberdn;
